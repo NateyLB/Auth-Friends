21fe931e2b5a9f9031ca7c37188feddb7d324d0d
Nathan <nateyl.b@gmail.com>
2020-04-23 09:54:57 -0700

    changed file name to client

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 const token =
   'esfeyJ1c2VySWQiOiJiMDhmODZhZi0zNWRhLTQ4ZjItOGZhYi1jZWYzOTA0NUIhkufemQifQ';
 
-  const path = require("path");
-//don't forget to npm install -s path
+//   const path = require("path");
+// //don't forget to npm install -s path
 
-app.use(express.static(path.resolve(__dirname, "client", "build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-}); 
+// app.use(express.static(path.resolve(__dirname, "client", "build")));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// }); 
 
 let nextId = 7;
 
