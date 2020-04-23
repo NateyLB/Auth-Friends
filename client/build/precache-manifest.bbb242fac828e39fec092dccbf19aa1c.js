d2fc2e49f090723e42330cf491b003cd5cd9b305
Nathan <nateyl.b@gmail.com>
2020-04-23 10:03:07 -0700

    chnaged file name

---
+++ client/build/precache-manifest.bbb242fac828e39fec092dccbf19aa1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d3c0de459bb3a838cd07b0290dc0d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1eea4521d2dd5ff8090",
+    "url": "/static/css/main.38cafea2.chunk.css"
+  },
+  {
+    "revision": "c4d5a9d49055757d43fd",
+    "url": "/static/js/2.b9f517c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9f517c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1eea4521d2dd5ff8090",
+    "url": "/static/js/main.2c43c1cc.chunk.js"
+  },
+  {
+    "revision": "81a90bdae97424d1eccf",
+    "url": "/static/js/runtime-main.c4e781c6.js"
+  }
+]);
